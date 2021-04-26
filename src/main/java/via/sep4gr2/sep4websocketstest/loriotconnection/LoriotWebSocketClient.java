7d1d2c55c55f3771398a56a3c40ec694262443d0
Toni <antonio.grama10@yahoo.com>
2021-04-26 16:57:49 +0200

    Updated client with working server url

--- src/main/java/via/sep4gr2/sep4websocketstest/loriotconnection/LoriotWebSocketClient.java
+++ src/main/java/via/sep4gr2/sep4websocketstest/loriotconnection/LoriotWebSocketClient.java
@@ -12,7 +12,7 @@ import java.util.concurrent.CompletionStage;
 public class LoriotWebSocketClient implements WebSocket.Listener, PropertyChangeSubject {
     private WebSocket server = null;
     private PropertyChangeSupport support = new PropertyChangeSupport(this);
-    private static final String SERVER_URL = "wss://iotnet.cibicom.dk/app?token=vnoTOQAAABFpb3RuZXQuY2liaWNvbS5kaxOhpkiCUsn0QwqoA8agq88=";
+    private static final String SERVER_URL = "wss://iotnet.cibicom.dk/app?token=vnoTuQAAABFpb3RuZXQuY2liaWNvbS5kaxJcKw-s3YsQp99sxC0vwhU=";
 
     public void sendDownLink(String jsonTelegram) {
         server.sendText(jsonTelegram,true);
