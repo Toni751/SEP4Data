4eaf702826581388e29870e7b1101a51ba08d9ce
jamborpal <jamborpal0@gmail.com>
2021-04-28 11:31:59 +0200

    tried doing the gardenbyownerid with the custom object
    not sure if its correct

--- src/main/java/via/sep4gr2/sep4websocketstest/repositories/DimGardenRepository.java
+++ src/main/java/via/sep4gr2/sep4websocketstest/repositories/DimGardenRepository.java
@@ -7,13 +7,17 @@ import via.sep4gr2.sep4websocketstest.models.database.DimPlant;
 
 import java.util.List;
 
-public interface DimGardenRepository extends JpaRepository<DimGarden, Integer> {
+public interface DimGardenRepository extends JpaRepository<DimGarden, Integer>
+{
 
     @Query("SELECT g FROM DimGarden g WHERE g.owner_google_id = ?1")
     List<DimGarden> findByOwner_google_id(String ownerId);
 
     @Query("SELECT g.sensor_ID FROM DimGarden g WHERE g.name = ?1")
     List<Integer> findAllSensorIdsByName(String name);
+
+    @Query("Select garden_ID from DimGarden where sensor_type=?1")
+    List<Integer> findGardensBySensorType(String sensorType);
 
     // no idea if we should query with the ? like above or with :attribute, but if both work, I think the ? version is more secure (sql injection-proof)
     @Query("SELECT DISTINCT 1 AS gardens FROM DimGarden g WHERE EXISTS (SELECT 1 FROM DimGarden gr WHERE gr.name = :name)")
