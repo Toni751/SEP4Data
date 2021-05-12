dc46051678f83dcf466ca120e2e52441a2b68863
Toni <antonio.grama10@yahoo.com>
2021-05-12 15:02:55 +0200

    Ci deploy attempt updated

--- src/main/java/via/sep4gr2/sep4websocketstest/controllers/HealthController.java
+++ src/main/java/via/sep4gr2/sep4websocketstest/controllers/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/")
 public class HealthController {
     @GetMapping
     public String getHealth(){
