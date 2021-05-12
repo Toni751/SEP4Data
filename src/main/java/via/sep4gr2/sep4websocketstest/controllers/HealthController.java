644e4b2c3c3b93f2a93db783040e213898ac018f
anaasima <58686057+anaasima@users.noreply.github.com>
2021-05-12 16:18:02 +0200

    buna

--- src/main/java/via/sep4gr2/sep4websocketstest/controllers/HealthController.java
+++ src/main/java/via/sep4gr2/sep4websocketstest/controllers/HealthController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping
     public String getHealth(){
-        return "hei!";
+        return "buna!";
     }
 }
